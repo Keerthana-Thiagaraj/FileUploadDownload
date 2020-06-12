bd5d86631ce3c975b1c747bfdb8a95f3df1c348d
keerthanacse13@gmail.com <keerthanacse13@gmail.com>
2020-06-13 00:35:26 +0530

    Downloadfile API exposed

--- src/main/java/springbootfileuploaddownload/controller/FileController.java
+++ src/main/java/springbootfileuploaddownload/controller/FileController.java
@@ -1,19 +1,22 @@
 package springbootfileuploaddownload.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 import springbootfileuploaddownload.constants.MessageConstants;
 import springbootfileuploaddownload.messages.ResponseMessage;
+import springbootfileuploaddownload.model.FileInfo;
 import springbootfileuploaddownload.service.FileService;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class FileController {
@@ -48,5 +51,30 @@ public class FileController {
 
 
     }
+
+    @GetMapping("/fetch")
+    public ResponseEntity<List<FileInfo>> getAllFiles() {
+
+        List<FileInfo> fileInfoList = fileService.loadAll().map(path -> {
+
+            String fileName = path.getFileName().toString();
+            String url = MvcUriComponentsBuilder.fromMethodName(FileController.class, "getFile",
+                    path.getFileName().toString()).build().toString();
+            return new FileInfo(fileName, url);
+
+        }).collect(Collectors.toList());
+
+        return ResponseEntity.status(HttpStatus.OK).body(fileInfoList);
+    }
+
+
+    @GetMapping("/fetch/{filename:.+}")
+    public ResponseEntity<Resource> getFile(@PathVariable String filename) {
+        Resource file = fileService.load(filename);
+        return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
+                "attachment;filename=\"" + file.getFilename() + "\"").body(file);
+
+    }
+
 
 }
